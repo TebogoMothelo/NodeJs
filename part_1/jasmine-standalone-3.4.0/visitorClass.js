9a5643ac6c3bf5c62c17849a287cd916aa9d787a
unknown <you@example.com>
2019-07-29 22:27:37 +0200

    modified class

--- part_1/jasmine-standalone-3.4.0/visitorClass.js
+++ part_1/jasmine-standalone-3.4.0/visitorClass.js
@@ -8,36 +8,30 @@ class visitor{
         this.time = time;
         this.comments = comments;
         this.assistorName = assistorName;
-        let o =  {
-            fullName,
-            age,
-            date,
-            time,
-            comments,
-            assistorName,
-        }  
-        visitorsArray.push(o)
-        return visitorsArray
     }
+    
+    save(){
+        visitorsArray.push(this)
+        let visitorsArray2 = visitorsArray
+        let fs = require('fs');
+        for(let i = 0; i<visitorsArray2.length; i++){
+            //add if statement 
+            let visitorFile = JSON.stringify(visitorsArray2[i])
+            fs.writeFile( `visitor_${i+1}.JSON`, visitorFile, function(err){
+                if(err) throw err;
+                console.log('worked');
+            })
+        }    
+    }
+    load(id){
+        return require(`./visitor_${id}.json`)
+       }
 
 }
 
-function save(){
-    let visitorsArray2 = visitorsArray
-    let fs = require('fs');
-    for(let i = 0; i<visitorsArray2.length; i++){
-        //add if statement 
-        let visitorFile = JSON.stringify(visitorsArray2[i])
-        fs.writeFile( "visitor_" + [i+1] + ".JSON", visitorFile, function(err){
-            if(err) throw err;
-            console.log('worked');
-        })
-    }    
-}
 
-function load(id){
-    return require("./visitor_"+id+".json")
-   }
+
+
 
 
 
